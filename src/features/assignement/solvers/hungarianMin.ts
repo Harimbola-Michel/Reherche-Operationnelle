87fb32c574b0d91a3d336e6296155aef33f400e3
LxrdShadow <andritiana.idealy@gmail.com>
2026-03-08 19:09:47 +0300

    feat(assignment): initialize primary variable for the algorithm

--- src/features/assignement/solvers/hungarianMin.ts
+++ src/features/assignement/solvers/hungarianMin.ts
@@ -1,7 +1,16 @@
 import { HungarianStep } from "../types/step";
+import { cloneMatrix, createBoolMatrix } from "../utils/matrixHelpers";
 
 export function solveHungarianMin(matrix: number[][]): HungarianStep[] {
     const steps: HungarianStep[] = [];
+
+    const n = matrix.length;
+    const m = cloneMatrix(matrix);
+
+    const framed = createBoolMatrix(n);
+    const crossed = createBoolMatrix(n);
+    const coveredRows = Array(n).fill(false);
+    const coveredCols = Array(n).fill(false);
 
     return steps;
 }
