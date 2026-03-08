8b99c7a46d65f91b0d79508119e48085f82dfb00
LxrdShadow <andritiana.idealy@gmail.com>
2026-03-08 19:00:09 +0300

    feat(assignment): initialize min solver function

---
+++ src/features/assignement/solvers/hungarianMin.ts
@@ -0,0 +1,7 @@
+import { HungarianStep } from "../types/step";
+
+export function solveHungarianMin(matrix: number[][]): HungarianStep[] {
+    const steps: HungarianStep[] = [];
+
+    return steps;
+}
